ffc12f1c3275c7420d45c6a5f0b5dbf5b4385f5f
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-09-11 07:01:42 +0600

    Assignment 04

--- problem5.js
+++ problem5.js
@@ -7,10 +7,17 @@ function  waitingTime(waitingTimes  , serialNumber) {
     for(const time of waitingTimes){
         sum += time;
     }
-    const avgTime = sum / waitingTimes.length;
+    const avgTime = Math.round(sum / waitingTimes.length)
+    const personSiNo = serialNumber - 1;
+    const personSiBefore = personSiNo - waitingTimes.length;
+    return personSiBefore * avgTime;
 }
 
 
 console.log(waitingTime([ 3, 5, 7, 11, 6 ], 10));
-console.log(waitingTime([7, 8, 3, 4, 5], "9"));
+console.log(waitingTime([13, 2], 6));
+console.log(waitingTime([13, 2, 6, 7, 10], 6));
+console.log(waitingTime([6], 4));
+console.log(waitingTime(7 , 10));
 console.log(waitingTime("[6,2]", 9));
+console.log(waitingTime([7, 8, 3, 4, 5], "9"));
